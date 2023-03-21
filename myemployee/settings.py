1c30e9d3196d598d67723cc26f237c1783a3fc78
github_username <nguyenvanmanh2001it1@gmail.com>
2023-03-21 19:43:40 +0700

    add

--- myemployee/settings.py
+++ myemployee/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wxxi7dyw)fy$wxpj9$m6v!y+r%$$5_do9-7gu+#bcpx6-^pbl8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
